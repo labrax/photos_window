9392ab37f2f2f312030f3f1b9b4679ee34e71f22
Victor <labrax@gmail.com>
2019-03-23 10:36:48 +0000

    initial commit

---
+++ photos.py
@@ -0,0 +1,25 @@
+
+
+import time
+import subprocess
+
+FOLDER = "/home/pi/photos_window/data/"
+DELAY_PHOTOS = 60 * 2 # 2 minutes
+
+def run():
+    start_time = time()
+    file = time.strftime("%Y%m%d_%H%M.image.jpg")
+    command = "raspistill -ex auto -fli auto -awb auto -q 25 -o {}{}".format(FOLDER, file)
+    subprocess.run(command, shell=True, check=False)
+    end_time = time()
+    time.sleep(DELAY_PHOTOS - end_end - start_time)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            run()
+        except Exception as e:
+            print(e)
+        time.sleep(5)
+
