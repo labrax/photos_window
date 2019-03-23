2346c82731045bcab77f8e05870f8072eeccc13f
labrax <labrax@gmail.com>
2019-03-23 11:42:30 +0100

    minor fixes

--- photos.py
+++ photos.py
@@ -7,12 +7,12 @@ FOLDER = "/home/pi/photos_window/data/"
 DELAY_PHOTOS = 60 * 2 # 2 minutes
 
 def run():
-    start_time = time()
+    start_time = time.time()
     file = time.strftime("%Y%m%d_%H%M.image.jpg")
     command = "raspistill -ex auto -fli auto -awb auto -q 25 -o {}{}".format(FOLDER, file)
     subprocess.run(command, shell=True, check=False)
-    end_time = time()
-    time.sleep(DELAY_PHOTOS - end_end - start_time)
+    end_time = time.time()
+    time.sleep(DELAY_PHOTOS - (end_time - start_time))
 
 
 if __name__ == '__main__':
---
+++ run.bash
@@ -0,0 +1,2 @@
+cd $(dirname $0)
+python3 photos.py >> log.out 2>> log.err &
