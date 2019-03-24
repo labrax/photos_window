b70fa68d75f2c98a2eefac62da54bb3f63e4bc81
Victor <labrax@gmail.com>
2019-03-24 12:52:27 +0000

    command used to generate video

--- gen.bash
+++ gen.bash
@@ -1,1 +1,22 @@
 https://askubuntu.com/questions/648244/how-do-i-create-an-animated-gif-from-still-images-preferably-with-the-command-l
+
+ffmpeg \
+  -framerate 10 \
+  -pattern_type glob \
+  -i '*.jpg' \
+  -r 15 \
+  -vf scale=512:-1 \
+  out.gif \
+;
+
+
+
+ffmpeg \
+  -framerate 10 \
+  -pattern_type glob \
+  -i '*.jpg' \
+  -vf scale=1024:-1 \
+  -b 2000k \
+  out.mp4 \
+;
+
