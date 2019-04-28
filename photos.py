b9b66fe13da64ad06d381e4a8d26fbc0d9c7df46
Victor <labrax@gmail.com>
2019-04-28 15:52:11 +0100

    photo resolution

--- photos.py
+++ photos.py
@@ -4,12 +4,12 @@ import time
 import subprocess
 
 FOLDER = "/home/pi/photos_window/data/"
-DELAY_PHOTOS = 60 * 2 # 2 minutes
+DELAY_PHOTOS = 60 * 15 # 15 minutes
 
 def run():
     start_time = time.time()
     file = time.strftime("%Y%m%d_%H%M.image.jpg")
-    command = "raspistill -ex auto -fli auto -awb auto -q 25 -o {}{}".format(FOLDER, file)
+    command = "raspistill -ex auto -fli auto -awb auto -q 25 -w 1920 -h 1080 -o {}{}".format(FOLDER, file)
     subprocess.run(command, shell=True, check=False)
     end_time = time.time()
     time.sleep(DELAY_PHOTOS - (end_time - start_time))
